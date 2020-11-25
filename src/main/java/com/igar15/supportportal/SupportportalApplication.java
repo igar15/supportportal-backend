41aec09b18f14836df79c09806b8bf5cff9d85aa
Igor Sh <igar15@yandex.ru>
2020-11-25 17:41:56 +0300

    Create FileConstant class;

--- src/main/java/com/igar15/supportportal/SupportportalApplication.java
+++ src/main/java/com/igar15/supportportal/SupportportalApplication.java
@@ -1,15 +1,20 @@
 package com.igar15.supportportal;
 
+import com.igar15.supportportal.constant.FileConstant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.io.File;
+
 
 @SpringBootApplication
 public class SupportportalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SupportportalApplication.class, args);
+		new File(FileConstant.USER_FOLDER).mkdirs();
 	}
 
 	@Bean
--- src/main/java/com/igar15/supportportal/service/UserService.java
+++ src/main/java/com/igar15/supportportal/service/UserService.java
@@ -1,6 +1,7 @@
 package com.igar15.supportportal.service;
 
 import com.igar15.supportportal.domain.User;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
 import java.util.List;
@@ -14,4 +15,14 @@ public interface UserService {
     User findUserByUsername(String username);
 
     User findUserByEmail(String email);
+
+    User addNewUser(String firstName, String lastName, String username, String email, String role, boolean isNonLocked, boolean isActive, MultipartFile profileImage);
+
+    User updateUser(String currentUsername, String newFirstName, String newLastName, String newUsername, String newEmail, String role, boolean isNonLocked, boolean isActive, MultipartFile profileImage);
+
+    void deleteUser(long id);
+
+    void resetPassword(String email);
+
+    User updateProfileImage(String username, MultipartFile profileImage);
 }
