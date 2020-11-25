1df50e9dc7dd718bd78a455d921ee67a8d51abe0
igar15 <igar15@yandex.ru>
2020-11-25 07:52:48 +0300

    Add login method to UserResource

--- src/main/java/com/igar15/supportportal/resource/UserResource.java
+++ src/main/java/com/igar15/supportportal/resource/UserResource.java
@@ -1,11 +1,17 @@
 package com.igar15.supportportal.resource;
 
+import com.igar15.supportportal.constant.SecurityConstant;
 import com.igar15.supportportal.domain.User;
+import com.igar15.supportportal.domain.UserPrincipal;
 import com.igar15.supportportal.exception.domain.EmailExistException;
 import com.igar15.supportportal.service.UserService;
+import com.igar15.supportportal.utility.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,6 +20,33 @@ public class UserResource {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody User user) {
+        authenticate(user.getUserName(), user.getPassword());
+        User loginUser = userService.findUserByUsername(user.getUserName());
+        UserPrincipal userPrincipal = new UserPrincipal(loginUser);
+        HttpHeaders jwtHeader = getJwtHeader(userPrincipal);
+        return new ResponseEntity<>(loginUser, jwtHeader, HttpStatus.OK);
+    }
+
+    private HttpHeaders getJwtHeader(UserPrincipal userPrincipal) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(SecurityConstant.JWT_TOKEN_HEADER, jwtTokenProvider.generateJwtToken(userPrincipal));
+        return httpHeaders;
+    }
+
+    private void authenticate(String userName, String password) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userName, password));
+    }
 
     @PostMapping("/register")
     public ResponseEntity<User> register(@RequestBody User user) {
