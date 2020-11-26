4b372d1533229768565fbe9a0aaf1b04926f7309
igar15 <igar15@yandex.ru>
2020-11-26 14:46:36 +0300

    Add add, update, find, list, resetPassword methods to UserResource

--- src/main/java/com/igar15/supportportal/resource/UserResource.java
+++ src/main/java/com/igar15/supportportal/resource/UserResource.java
@@ -1,6 +1,7 @@
 package com.igar15.supportportal.resource;
 
 import com.igar15.supportportal.constant.SecurityConstant;
+import com.igar15.supportportal.domain.HttpResponse;
 import com.igar15.supportportal.domain.User;
 import com.igar15.supportportal.domain.UserPrincipal;
 import com.igar15.supportportal.exception.domain.EmailExistException;
@@ -13,8 +14,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -54,6 +59,61 @@ public class UserResource {
     public ResponseEntity<User> register(@RequestBody User user) throws MessagingException {
         User newUser = userService.register(user.getFirstName(), user.getLastName(), user.getUserName(), user.getEmail());
         return new ResponseEntity<>(newUser, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<User> addNewUser(@RequestParam("firstName") String firstName,
+                                           @RequestParam("lastName") String lastName,
+                                           @RequestParam("username") String username,
+                                           @RequestParam("email") String email,
+                                           @RequestParam("role") String role,
+                                           @RequestParam("isActive") String isActive,
+                                           @RequestParam("isNonLocked") String isNonLocked,
+                                           @RequestParam(value = "profileImage", required = false) MultipartFile profileImage) throws IOException {
+        User newUser = userService.addNewUser(firstName, lastName, username, email, role,
+                Boolean.parseBoolean(isNonLocked), Boolean.parseBoolean(isActive), profileImage);
+        return new ResponseEntity<>(newUser, HttpStatus.OK);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity<User> update(@RequestParam("currentUsername") String currentUsername,
+                                           @RequestParam("firstName") String firstName,
+                                           @RequestParam("lastName") String lastName,
+                                           @RequestParam("username") String username,
+                                           @RequestParam("email") String email,
+                                           @RequestParam("role") String role,
+                                           @RequestParam("isActive") String isActive,
+                                           @RequestParam("isNonLocked") String isNonLocked,
+                                           @RequestParam(value = "profileImage", required = false) MultipartFile profileImage) throws IOException {
+        User updatedUser = userService.updateUser(currentUsername, firstName, lastName, username, email, role,
+                Boolean.parseBoolean(isNonLocked), Boolean.parseBoolean(isActive), profileImage);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{username}")
+    public ResponseEntity<User> getUser(@PathVariable("username") String username) {
+        User user = userService.findUserByUsername(username);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @GetMapping("/list")
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @GetMapping("/resetPassword/{email}")
+    public ResponseEntity<HttpResponse> resetPassword(@PathVariable("email") String email) throws MessagingException {
+        userService.resetPassword(email);
+        return response(HttpStatus.OK, "The email with your new password was send to " + email);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<HttpResponse>
+
+
+    private ResponseEntity<HttpResponse> response(HttpStatus status, String message) {
+
     }
 
 }
