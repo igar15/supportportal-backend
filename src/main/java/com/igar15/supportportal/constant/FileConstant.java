2add576a9f627ea133413682ec8b7a13b5ccf290
igar15 <igar15@yandex.ru>
2021-01-09 16:10:40 +0300

    Refactor FileConstant -> change DEFAULT_USER_IMAGE_PATH constant;

--- src/main/java/com/igar15/supportportal/constant/FileConstant.java
+++ src/main/java/com/igar15/supportportal/constant/FileConstant.java
@@ -1,11 +1,11 @@
 package com.igar15.supportportal.constant;
 
 public class FileConstant {
-    public static final String USER_IMAGE_PATH = "/user/image/";
+    public static final String USER_IMAGE_PATH = "/users/image/";
     public static final String JPG_EXTENSION = "jpg";
     public static final String USER_FOLDER = System.getProperty("user.home") + "/supportportal/user/";
     public static final String DIRECTORY_CREATED = "Created directory for: ";
-    public static final String DEFAULT_USER_IMAGE_PATH = "/user/image/profile/";
+    public static final String DEFAULT_USER_IMAGE_PATH = "/users/image/profile/";
     public static final String FILE_SAVED_IN_FILE_SYSTEM = "Saved file in file system by name: ";
     public static final String DOT = ".";
     public static final String FORWARD_SLASH = "/";
